ab11a6eb7259984fb673eee8f006eb76b57b4776
goph-R <gopher.hu@gmail.com>
2019-12-22 17:06:34 +0100

    Install.sql added.

--- domain/DbRouteAlias.php
+++ domain/DbRouteAlias.php
@@ -2,16 +2,6 @@
 
 class DbRouteAlias extends Record {
 
-/*
-CREATE TABLE `route_alias` (
-	`id` INT NOT NULL AUTO_INCREMENT,
-	`alias` VARCHAR(255) NOT NULL,
-	`path` VARCHAR(255) NOT NULL,
-	PRIMARY KEY (`id`)
-)
-COLLATE='utf8mb4_general_ci';
-*/
-
     protected $tableName = 'route_alias';
 
     protected $id;
